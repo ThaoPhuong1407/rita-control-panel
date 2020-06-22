6b41ce6fc75a86f0ef8bc6e4f04d734fc4f89a7d
Thao Phuong <thao@dollabs.com>
2020-06-22 03:10:05 -0400

    update cl

--- controllers/frontend/stateEstimation.js
+++ controllers/frontend/stateEstimation.js
@@ -9,7 +9,6 @@ const socketFrontEnd = io.connect('http://localhost:3000/state-estimation');
 /* ----------------------------*/
 /* Efficient Level Graph (EL)  */
 /* ----------------------------*/
-
 // Initialize variables
 var elGraph = document.getElementById('el-graph');
 var predictionID = [];
@@ -125,17 +124,79 @@ socketFrontEnd.on('newPrediction', function (data) {
   }
 });
 
-/* ----------------------------*/
-/* Cognitive Load Graph (CL)  */
-/* ----------------------------*/
+/* -------------------------------------------------------*/
+/* Efficient Level Graph (EL) && Memory Count Graph (MC) */
+/* ------------------------------------------------------*/
 var clGraph = document.getElementById('cl-graph');
+var mcGraph = document.getElementById('mc-graph');
 
-// Initialize graph
-// Note: Untriaged Green Victims = 9 CL
-var xValue = ['Cognitive load', 'Rooms Skipped', 'Untriaged Urgent Victims', 'Untriaged Green Victims'];
-var yValue = [0, 0, 0, 0];
-
+// Initialize graph: Cognitive Level Graph (CL)
 var dataCL = [
+  {
+    y: [0],
+  },
+];
+
+var layoutCL = {
+  hovermode: 'closest',
+  title: {
+    text: 'Cognitive Load',
+    font: {
+      family: 'Courier New, monospace',
+      size: 30,
+      color: '#ffff',
+    },
+  },
+  xaxis: {
+    linecolor: '#228DFF',
+    color: '#ffe6ffd2',
+    tickformat: ',d',
+    title: {
+      text: 'Unit of time',
+      font: {
+        family: 'monospace',
+        size: 18,
+        color: '#f9e3ff',
+      },
+      tickcolor: '#228DFF',
+    },
+    rangemode: 'nonnegative',
+    showspikes: true,
+    showgrid: false,
+  },
+  yaxis: {
+    linecolor: '#228DFF',
+    color: '#ffe6ffd2',
+    title: {
+      text: 'Bits',
+      font: {
+        family: 'monospace',
+        size: 18,
+        color: '#f9e3ff',
+      },
+    },
+    range: [0, 150],
+    rangemode: 'nonnegative',
+    showspikes: true,
+    showgrid: false,
+  },
+
+  paper_bgcolor: 'rgb(0,0,0,0)',
+  plot_bgcolor: 'rgb(0,0,0,0)',
+};
+
+var configCL = {
+  scrollZoom: true,
+  responsive: true,
+};
+
+Plotly.plot(clGraph, dataCL, layoutCL, configCL);
+
+// Initialize graph: Memory Count Graph (MC)
+var xValue = ['Rooms Skipped', 'Untriaged Urgent Victims', 'Untriaged Green Victims'];
+var yValue = [0, 0, 0];
+
+var dataMC = [
   {
     x: xValue,
     y: yValue,
@@ -150,19 +211,19 @@ var dataCL = [
   },
 ];
 
-var layoutCL = {
+var layoutMC = {
   title: {
-    text: 'Cognitive Load',
+    text: 'Memory Piece',
     font: {
-      family: 'Courier New, monospace',
-      size: 35,
+      family: 'Bellota, monospace',
+      size: 30,
       color: '#ffff',
     },
   },
   xaxis: {
     linecolor: '#ffe6ffd2',
     tickfont: {
-      size: 11,
+      size: 13,
       color: '#ffe6ffd2',
     },
   },
@@ -179,26 +240,38 @@ var layoutCL = {
     },
     showspikes: true,
     showgrid: false,
-    range: [0, 180],
+    range: [0, 50],
   },
 
   paper_bgcolor: 'rgb(0,0,0,0)',
   plot_bgcolor: 'rgb(0,0,0,0)',
 };
 
-var configCL = {
+var configMC = {
   scrollZoom: true,
   responsive: true,
 };
 
-Plotly.plot(clGraph, dataCL, layoutCL, configCL);
+Plotly.plot(mcGraph, dataMC, layoutMC, configMC);
 
-// Update graph based on incoming dataCL
+// Update graphes based on incoming data
 var updatedValue;
 socketFrontEnd.on('newCognitiveLoad', function (data) {
+  // CL graph
+  Plotly.extendTraces(clGraph, { y: [[data.cognitiveLoad]] }, [0]);
+
+  // MC graph
   setTimeout(function () {
-    console.log(data);
-    updatedValue = [data.cognitiveLoad, data.roomsSkipped, data.unUrgentPatients, data.unGreenPatients];
-    Plotly.restyle(clGraph, 'y', [updatedValue]);
+    updatedValue = [data.roomsSkipped, data.unUrgentPatients, data.unGreenPatients];
+    Plotly.restyle(mcGraph, 'y', [updatedValue]);
+    $(`#cognitive_load`).text(`${data.cognitiveLoad} bits`);
+    $(`#room_skipped`).text(`${data.roomsSkipped} room(s)`);
+    $(`#urgent_patient`).text(`${data.unUrgentPatients} patient(s)`);
+    $(`#green_patient`).text(`${data.unGreenPatients} patient(s)`);
+
+    $(`#cognitive_load1`).text(`${data.cognitiveLoad} bits`);
+    $(`#room_skipped1`).text(`${data.roomsSkipped} room(s)`);
+    $(`#urgent_patient1`).text(`${data.unUrgentPatients} patient(s)`);
+    $(`#green_patient1`).text(`${data.unGreenPatients} patient(s)`);
   }, 300);
 });
